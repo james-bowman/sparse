3ff7d68117de30a4cd370106bd924b09b99b9d68
james-bowman <james.edward.bowman@gmail.com>
2018-04-05 23:01:39 +0100

    added blas

---
+++ blas/doc.go
@@ -0,0 +1,5 @@
+/*
+Package blas provides implementations of sparse BLAS (Basic Linear Algebra Subprograms) routines
+for sparse matrix arithmetic and solving sparse linear systems.
+*/
+package blas
