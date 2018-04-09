b167526006812e6c5c960553daa2dc7d0e24bb50
james-bowman <james.edward.bowman@gmail.com>
2018-04-09 23:14:06 +0100

    updated with link to documentation

--- blas/doc.go
+++ blas/doc.go
@@ -1,5 +1,7 @@
 /*
 Package blas provides implementations of sparse BLAS (Basic Linear Algebra Subprograms) routines
 for sparse matrix arithmetic and solving sparse linear systems.
+
+See http://www.netlib.org/blas/blast-forum/chapter3.pdf for further information.
 */
 package blas
