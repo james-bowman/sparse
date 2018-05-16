60f6f25f04957703336e07524b048351ef43ea65
james-bowman <james.edward.bowman@gmail.com>
2018-05-16 22:30:14 +0100

    made dot result a named return variable

--- blas/level1.go
+++ blas/level1.go
@@ -3,12 +3,11 @@ package blas
 // Dusdot (Sparse dot product (r <- x^T * y)) calculates the dot product of
 // sparse vector x and dense vector y.  indx is used as the index
 // values to gather and incy as the stride for y.
-func Dusdot(x []float64, indx []int, y []float64, incy int) float64 {
-	var dot float64
+func Dusdot(x []float64, indx []int, y []float64, incy int) (dot float64) {
 	for i, index := range indx {
 		dot += x[i] * y[index*incy]
 	}
-	return dot
+	return
 }
 
 // Dusaxpy (Sparse update (y <- alpha * x + y)) scales the sparse vector x by
