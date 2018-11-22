c127ec9590274b651ce0af634ff57f8bd6c76b7c
james-bowman <james.edward.bowman@gmail.com>
2018-11-22 18:25:21 +0000

    corrected indents

--- blas/dot_amd64.s
+++ blas/dot_amd64.s
@@ -5,33 +5,33 @@
 // func Dusdot(x []float64, indx []int, y []float64, incy int) (dot float64)
 TEXT ·Dusdot(SB), NOSPLIT, $0
     MOVQ    x+0(FP), R8
-	MOVQ    indx+24(FP), SI
+    MOVQ    indx+24(FP), SI
     MOVQ    indx+32(FP), AX     // AX = len(indx)
     MOVQ    y+48(FP), DX
     MOVQ    incy+72(FP), CX
-    
-	XORL	R9, R9              // i = 0
-	XORPS	X0, X0              // sum = 0.0
+
+    XORL    R9, R9              // i = 0
+    XORPS   X0, X0              // sum = 0.0
 
     SUBQ    $1, AX              // len(indx)--
 
 loop:
-    CMPQ	R9, AX              // for ;i < len(indx);
-	JGE	    tail
+    CMPQ    R9, AX              // for ;i < len(indx);
+    JGE     tail
 
-    MOVUPD	(R8)(R9*8), X1      // X1 := x[i : i+1]
+    MOVUPD  (R8)(R9*8), X1      // X1 := x[i : i+1]
     
-    MOVQ	(SI)(R9*8), R10     // R10 := indx[i]
-    IMULQ	CX, R10             // R10 *= incy
-    INCQ	R9                  // i++
+    MOVQ    (SI)(R9*8), R10     // R10 := indx[i]
+    IMULQ   CX, R10             // R10 *= incy
+    INCQ    R9                  // i++
     MOVLPD  (DX)(R10*8), X2     // X2l = y[R10]
 
-    MOVQ	(SI)(R9*8), R10     // R10 := indx[i]
-    IMULQ	CX, R10             // R10 *= incy
-    INCQ	R9                  // i++
+    MOVQ    (SI)(R9*8), R10     // R10 := indx[i]
+    IMULQ   CX, R10             // R10 *= incy
+    INCQ    R9                  // i++
     MOVHPD  (DX)(R10*8), X2     // X2h = y[R10]
-    
-    MULPD	X2, X1              
+
+    MULPD   X2, X1              
     ADDPD   X1, X0              // X0 += X1 * X2
 
     JMP     loop
@@ -40,7 +40,7 @@ tail:
     ADDQ    $1, AX
     CMPQ    R9, AX
     JGE     end
-    
+
     // Sum product of last elements if odd number of elements
     MOVSD   (R8)(R9*8), X1      // X1 := x[i]
 
@@ -50,10 +50,10 @@ tail:
     ADDSD   X1, X0
 
 end:
-	// Add the two sums together.
-	MOVSD   X0, X3
-	UNPCKHPD X0, X0
-	ADDSD   X3, X0
+    // Add the two sums together.
+    MOVSD   X0, X3
+    UNPCKHPD X0, X0
+    ADDSD   X3, X0
 
     MOVSD   X0, dot+80(FP)      // Return final sum.
     RET
