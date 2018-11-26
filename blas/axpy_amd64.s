4a74a94aeaadf5bf28ce102524edaff7ee7f8643
james-bowman <james.edward.bowman@gmail.com>
2018-11-26 22:48:18 +0000

    added further unrolling to loop and reduced dependency chains

--- blas/axpy_amd64.s
+++ blas/axpy_amd64.s
@@ -12,51 +12,69 @@ TEXT ·Dusaxpy(SB), NOSPLIT, $0
     MOVQ    incy+80(FP), DI
 
     SHUFPD  $0, X0, X0
+    MOVUPD  X0, X1              // copy alpha for better pipelining
 
     XORL    R9, R9
 
-    DECQ    AX                  // len(indx)--
+    SUBQ    $3, AX              // for ;len(indx) - 3;  // x4 loop unrolling
 
 loop:
     CMPQ    R9, AX
-    JGE     tail
+    JGE     tailstart
 
-    MOVUPD  (SI)(R9*8), X2      // X1 := x[i : i+1]
+    MOVUPD  (SI)(R9*8), X2      // X2 := x[i : i+1]
+    MOVUPD  16(SI)(R9*8), X3    // X3 := x[i+2 : i+4]
 
     MOVQ    (R8)(R9*8), R10     // R10 := indx[i]
     MOVQ    8(R8)(R9*8), R11    // R11 := indx[i+1]
+    MOVQ    16(R8)(R9*8), R12   // R12 := indx[i+2]
+    MOVQ    24(R8)(R9*8), R13   // R13 := indx[i+2]
  
     IMULQ   DI, R10             // R10 *= incy
     IMULQ   DI, R11             // R11 *= incy
+    IMULQ   DI, R12             // R12 *= incy
+    IMULQ   DI, R13             // R13 *= incy
 
-    MOVLPD  (CX)(R10*8), X1     // X2l = y[R10]
-    MOVHPD  (CX)(R11*8), X1     // X2h = y[R11]
+    MOVLPD  (CX)(R10*8), X4     // X4l = y[R10]
+    MOVHPD  (CX)(R11*8), X4     // X4h = y[R11]
+    MOVLPD  (CX)(R12*8), X5     // X5l = y[R12]
+    MOVHPD  (CX)(R13*8), X5     // X5h = y[R13]
 
     MULPD   X0, X2              // X2 := alpha * x[i : i+1]
-    ADDPD   X2, X1              // X1 := y + X2
+    MULPD   X1, X3 
 
-    MOVLPD  X1, (CX)(R10*8)
-    MOVHPD  X1, (CX)(R11*8)
+    ADDPD   X4, X2
+    ADDPD   X5, X3
 
-    ADDQ    $2, R9              // i += 2
+    MOVLPD   X2, (CX)(R10*8)
+    MOVHPD   X2, (CX)(R11*8)
+    MOVLPD   X3, (CX)(R12*8)
+    MOVHPD   X3, (CX)(R13*8)
+
+    ADDQ    $4, R9              // i += 2
 
     JMP     loop
 
+tailstart:
+    ADDQ    $3, AX
+
 tail:
-    INCQ    AX
     CMPQ    R9, AX
     JGE     end
 
-    // one more y[indx[i]*incy] += alpha * x[i]
+    // y[indx[i]*incy] += alpha * x[i] for remaining elements of x
     MOVSD   (SI)(R9*8), X2      // X1 := x[i : i+1]
 
     MOVQ    (R8)(R9*8), R10     // R10 := indx[i]
     IMULQ   DI, R10             // R10 *= incy
-    MOVSD   (CX)(R10*8), X1
 
     MULSD   X0, X2
-    ADDSD   X2, X1
-    MOVSD   X1, (CX)(R10*8)
+    ADDSD   (CX)(R10*8), X2
+    MOVSD   X2, (CX)(R10*8)
+
+    INCQ    R9
+
+    JMP     tail
 
 end:
     RET
