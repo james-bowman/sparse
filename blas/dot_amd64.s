9d9af4a6a2c4d8f669a1bac7f1bb083b30644b0a
james-bowman <james.edward.bowman@gmail.com>
2019-01-03 08:30:05 +0000

    use software pipelining and loop inversion optimisation

--- blas/dot_amd64.s
+++ blas/dot_amd64.s
@@ -5,78 +5,72 @@
 // func Dusdot(x []float64, indx []int, y []float64, incy int) (dot float64)
 TEXT ·Dusdot(SB), NOSPLIT, $0
     MOVQ    x+0(FP), R8
-    MOVQ    indx+24(FP), SI
-    MOVQ    indx+32(FP), AX     // AX = len(indx)
-    MOVQ    y+48(FP), DX
-    MOVQ    incy+72(FP), CX
+	MOVQ    indx+24(FP), SI    
+    MOVQ    y+48(FP), DX       
+    MOVQ    indx+32(FP), AX             // len(indx)
+    MOVQ    incy+72(FP), CX 
+    
+	XORPS	X0, X0                      // 2 accumulators for reduced dependencies and
+	XORPS	X9, X9                      // better software pipelining
 
-    XORL    R9, R9              // i = 0
-    XORPS   X0, X0              // sum = 0.0
-    XORPS   X1, X1              // sum2 = 0.0
-
-    SUBQ    $4, AX              // len(indx)-4
- 
-    LEAQ    (SI)(AX*8), R14     // R14 = &indx[len(indx)-4]
-    LEAQ    (R8)(AX*8), R15     // R15 = &indx[len(indx)-4]
-
-    SUBQ    AX, R9
-    JG      tailstart
+    SUBQ    $4, AX
+	JL	    tailstart
 
 loop:
-    MOVUPD  (R15)(R9*8), X2     // X1 := x[i : i+1]
-    MOVUPD  16(R15)(R9*8), X3   // X1 := x[i+2 : i+3]
+    MOVQ	(SI), R10           
+    MOVQ	8(SI), R11           
+    MOVQ	16(SI), R12           
+    MOVQ	24(SI), R13           
 
-    // For larger lengths of x and indx, this ordering of the unwound
-    // operations is actually more performant than typical
-    // ordering e.g. all the MOVQ ops, followed by all the IMULQ, ... 
+	MOVUPD	(R8), X1                    // load packed pairs of elements from x into SSE registers
+	MOVUPD	16(R8), X3       
 
-    MOVQ    (R14)(R9*8), R10    // R10 := indx[i]
-    IMULQ   CX, R10             // R10 *= incy
-    MOVLPD  (DX)(R10*8), X4     // X4l = y[R10]
+    IMULQ	CX, R10                     // multiply indx (indices of x) by incy (stride for y) to
+    IMULQ	CX, R11                     // calculate corresponding indexes for y
+    IMULQ	CX, R12      
+    IMULQ	CX, R13      
 
-    MOVQ    8(R14)(R9*8), R11   // R11 := indx[i+1]
-    IMULQ   CX, R11             // R11 *= incy
-    MOVHPD  (DX)(R11*8), X4     // X4h = y[R11]
+ 	MOVLPD	(DX)(R10*8), X2             // pack scattered elements of y into SSE registers (gather)
+ 	MOVHPD	(DX)(R11*8), X2
+ 	MOVLPD	(DX)(R12*8), X4
+ 	MOVHPD	(DX)(R13*8), X4
 
-    MOVQ    16(R14)(R9*8), R12  // R12 := indx[i+2]
-    IMULQ   CX, R12             // R12 *= incy
-    MOVLPD  (DX)(R12*8), X5     // X5l = y[R10]
+	MULPD	X2, X1                      
+	MULPD	X4, X3
 
-    MOVQ    24(R14)(R9*8), R13  // R13 := indx[i+3]
-    IMULQ   CX, R13             // R13 *= incy
-    MOVHPD  (DX)(R13*8), X5     // X5h = y[R11]
+	ADDPD	X1, X0
+	ADDPD	X3, X9
 
-    MULPD   X4, X2
-    MULPD   X5, X3    
+    ADDQ    $32, SI                     // increment pointers by 4*sizeOf(float64)
+    ADDQ    $32, R8
 
-    ADDPD   X2, X0              // X0 += X4 * X2
-    ADDPD   X3, X1              // X1 += X5 * X3
-
-    ADDQ    $4, R9              // i += 4
-    JLE     loop
+    SUBQ    $4, AX
+    JGE     loop
 
 tailstart:
-    SUBQ    $4, R9
-    JNS     end
+    ADDQ    $4, AX
+    JE     	end
 
 tail:
-    // Sum product of last elements if number of elements is not divisible
-    // by 4
-    MOVSD   32(R15)(R9*8), X2   // X2 = x[i]
-    MOVQ    32(R14)(R9*8), R10  // R10 = indx[i]
-    IMULQ   CX, R10             // R10 *= incy
-    MULSD   (DX)(R10*8), X2     // X2 = x[i] * y[indx[i]*incy]
-    ADDSD   X2, X0              // sum += X2
+    // process remaining elements individually where length is not divisible by 4
+    MOVQ	(SI), R10           
+	MOVSD	(R8), X1      
+    IMULQ	CX, R10            
 
-    ADDQ    $1, R9  
-    JS      tail
+	MULSD	(DX)(R10*8), X1
+	ADDSD	X1, X0
+
+    ADDQ    $8, SI
+    ADDQ    $8, R8
+
+    SUBQ    $1, AX
+    JNE     tail
 
 end:
-    // Add the two accumulators together.
-    ADDPD   X1, X0
-    MOVSD   X0, X7
+    ADDPD   X9, X0                      // add accumulators together
+    MOVSD   X0, X7                      // unpack SSE register and add 2 values together
     UNPCKHPD X0, X0
     ADDSD   X7, X0
 
-    MOVSD   X0, dot+80(FP)      // Return final sum.
+    MOVSD   X0, dot+80(FP)      
     RET
